930676a2bb6651325c70b791c063b5256ce4407e
Gaurav Bankar <gaurav210233@gmail.com>
2023-11-20 23:32:41 +0530

    Removed hazardous function eval added safe way to evaluate expression

--- src/js/calc.js
+++ src/js/calc.js
@@ -1,9 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
-
+    
     let displayValue = document.getElementById('calc-input');
     const buttons = document.getElementsByClassName('btn');
 
     let currentValue = "";
+
+    function evaluateExpression(expression) {
+        try {
+            const result = new Function('return ' + expression)();
+            return result;
+        } catch (error) {
+            return 'ERROR';
+        }
+    }
 
     function handleButtonInput(value) {
         try {
@@ -14,13 +23,13 @@ document.addEventListener('DOMContentLoaded', function () {
             } else if (value === "=") {
                 if (currentValue == "") {
                     displayValue.value = "";
-                    return;
+                    return
                 }
                 convertedValue = currentValue
                     .replace("×", "*")
                     .replace("÷", "/")
                     .replace("%", "*0.01");
-                displayValue.value = String(eval(convertedValue));
+                displayValue.value = String(evaluateExpression(convertedValue));
 
             } else if (value === "⌫") {
                 currentValue = currentValue.slice(0, -1);;
